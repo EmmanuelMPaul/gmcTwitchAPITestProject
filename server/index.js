a7b72a1b1becda23090b6f7a2073ab95ee8c30b9
Emmanuel Paul <pauldukz@gmail.com>
2019-09-01 09:08:30 +0300

    added static files

--- server/index.js
+++ server/index.js
@@ -13,6 +13,11 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/api/channels', channels);
-
+//handle production
+if(process.env.NODE_ENV == 'production'){
+    //static folder
+    app.use(express.static(__dirname + '/public'));
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server started on port ${port} `));
